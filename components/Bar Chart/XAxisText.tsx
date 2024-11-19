4db86a4add365538a2f18777adb9b98a2c60fb0e
AnthonyBeerus <anthony.morima@gmail.com>
2024-11-20 01:08:45 +0200

    feat(XAxisText): implement dynamic text color based on selection
    - Added dynamic text color for X-axis labels based on the selected bar
    - Utilized `useDerivedValue` and `withTiming` for smooth color transitions
    - Integrated `useThemeColor` for theme-based color adaptation
    - Applied custom font (`SpaceMono-Regular`) for text rendering

---
+++ components/Bar Chart/XAxisText.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Text, useFont } from "@shopify/react-native-skia";
+import {
+  SharedValue,
+  useDerivedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+type Props = {
+  x: number;
+  y: number;
+  text: string;
+  selectedBar: SharedValue<string | null>;
+};
+
+const XAxisText = ({ x, y, text, selectedBar }: Props) => {
+  const font = useFont(require("../../assets/fonts/SpaceMono-Regular.ttf"), 18);
+
+  const activeColor = useThemeColor(
+    { light: "#111111", dark: "#FFFFFF" }, // Active text color
+    "text"
+  );
+  const mutedColor = useThemeColor(
+    { light: "#d1d0c5", dark: "#777777" }, // Muted text color
+    "muted"
+  );
+
+  const color = useDerivedValue(() => {
+    if (selectedBar.value === text) {
+      return withTiming(activeColor);
+    } else if (selectedBar.value === null) {
+      return withTiming(activeColor);
+    } else {
+      return withTiming(mutedColor);
+    }
+  });
+
+  if (!font) {
+    return null;
+  }
+
+  const fontSize = font.measureText(text);
+
+  return (
+    <Text
+      font={font}
+      x={x - fontSize.width / 2}
+      y={y}
+      text={text}
+      color={color}
+    />
+  );
+};
+
+export default XAxisText;
