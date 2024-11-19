86c8f383b1afc715ee02629e279f4675c72955ea
AnthonyBeerus <anthony.morima@gmail.com>
2024-11-20 01:00:44 +0200

    feat(auth): add new AuthScreen with dynamic form and themed components
    - Implemented a new `AuthScreen` for user authentication
    - Integrated `ThemedText` and `ThemedTextInput` for consistent theming
    - Added form state management for `email` and `password` fields
    - Included basic structure for "Forgot password?" and "Sign up" actions
    - Used `ParallaxScrollView` and `KeyboardAwareScrollView` for enhanced UX

---
+++ app/index.tsx
@@ -0,0 +1,229 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedTextInput } from "@/components/ThemedTextInput";
+import { ThemedView } from "@/components/ThemedView";
+import { useColorScheme } from "@/hooks/useColorScheme.web";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+export default function AuthScreen() {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+    const colorScheme = useColorScheme();
+
+    const buttonBrandColor = colorScheme === "dark" ? "#F04444" : "#075eec";
+
+    return (
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <GestureHandlerRootView>
+          <ParallaxScrollView
+            headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}>
+            <KeyboardAwareScrollView>
+              <ThemedView style={styles.header}>
+                <Image
+                  alt="App Logo"
+                  resizeMode="contain"
+                  style={styles.headerImg}
+                  source={require("../assets/images/brand-logo.png")}
+                />
+
+                <ThemedText type="title">
+                  Sign in to <Text style={{ color: "#F04444" }}>KAIZEN</Text>
+                </ThemedText>
+
+                <ThemedText type="subtitle">
+                  Get access to your portfolio and more
+                </ThemedText>
+              </ThemedView>
+
+              <ThemedView style={styles.form}>
+                {/* <View style={styles.input}>
+                  <Text style={styles.inputLabel}>Email address</Text>
+
+                  <TextInput
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    clearButtonMode="while-editing"
+                    keyboardType="email-address"
+                    onChangeText={(email) => setForm({ ...form, email })}
+                    placeholder="john@example.com"
+                    placeholderTextColor="#6b7280"
+                    style={styles.inputControl}
+                    value={form.email}
+                  />
+                </View>
+
+                <View style={styles.input}>
+                  <Text style={styles.inputLabel}>Password</Text>
+
+                  <TextInput
+                    autoCorrect={false}
+                    clearButtonMode="while-editing"
+                    onChangeText={(password) => setForm({ ...form, password })}
+                    placeholder="********"
+                    placeholderTextColor="#6b7280"
+                    style={styles.inputControl}
+                    secureTextEntry={true}
+                    value={form.password}
+                  />
+                </View> */}
+                <View style={styles.input}>
+                  <ThemedText>Password</ThemedText>
+                  <ThemedTextInput type="password" />
+                </View>
+                <View style={styles.input}>
+                  <ThemedText>Email Address</ThemedText>
+                  <ThemedTextInput type="email" />
+                </View>
+
+                <View></View>
+
+                <View style={styles.formAction}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      // handle onPress
+                    }}>
+                    <View style={styles.btn}>
+                      <Text style={styles.btnText}>Sign in</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity
+                  onPress={() => {
+                    // handle link
+                  }}>
+                  <Text style={styles.formLink}>Forgot password?</Text>
+                </TouchableOpacity>
+              </ThemedView>
+            </KeyboardAwareScrollView>
+
+            <TouchableOpacity
+              onPress={() => {
+                // handle link
+                
+              }}>
+              <ThemedText style={styles.formFooter}>
+                Don't have an account?{" "}
+                <Text style={{ textDecorationLine: "underline" }}>Sign up</Text>
+              </ThemedText>
+            </TouchableOpacity>
+          </ParallaxScrollView>
+        </GestureHandlerRootView>
+      </ThemeProvider>
+    );
+    }
+
+    const styles = StyleSheet.create({
+      container: {
+        paddingVertical: 24,
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 0,
+      },
+      title: {
+        fontSize: 31,
+        fontWeight: "700",
+        marginBottom: 6,
+      },
+      subtitle: {
+        fontSize: 15,
+        fontWeight: "500",
+        color: "#929292",
+      },
+      /** Header */
+      header: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 36,
+      },
+      headerImg: {
+        width: 200,
+        height: 80,
+        alignSelf: "center",
+        marginBottom: 36,
+        tintColor: "#F04444",
+      },
+      /** Form */
+      form: {
+        marginBottom: 24,
+        paddingHorizontal: 24,
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: 0,
+      },
+      formAction: {
+        marginTop: 4,
+        marginBottom: 16,
+      },
+      formLink: {
+        fontSize: 16,
+        fontWeight: "600",
+        color: "#F04444",
+        textAlign: "center",
+      },
+      formFooter: {
+        paddingVertical: 24,
+        fontSize: 15,
+        fontWeight: "600",
+        textAlign: "center",
+        letterSpacing: 0.15,
+      },
+      /** Input */
+      input: {
+        marginBottom: 16,
+      },
+      inputLabel: {
+        fontSize: 17,
+        fontWeight: "600",
+        color: "#222",
+        marginBottom: 8,
+      },
+      inputControl: {
+        height: 50,
+        backgroundColor: "#fff",
+        paddingHorizontal: 16,
+        borderRadius: 12,
+        fontSize: 15,
+        fontWeight: "500",
+        color: "#222",
+        borderWidth: 1,
+        borderColor: "#C9D3DB",
+        borderStyle: "solid",
+      },
+      /** Button */
+      btn: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        backgroundColor: "#F04444",
+        borderColor: "#F04444",
+      },
+      btnText: {
+        fontSize: 18,
+        lineHeight: 26,
+        fontWeight: "600",
+        color: "#fff",
+      },
+    });
