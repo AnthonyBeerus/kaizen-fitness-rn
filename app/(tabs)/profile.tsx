fc021ecf277e8f9d2764d176bfffa931a96d485c
AnthonyBeerus <anthony.morima@gmail.com>
2024-12-14 21:28:34 +0200

    stuff

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, Pressable, View } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { TwoColumnLayout } from "@/components/TwoColumnLayout";
@@ -7,6 +7,15 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { ThemedMainContainer } from "@/components/containers/ThemedMainContainerx";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { Colors } from "@/constants/Colors"; // Import the Colors for light and dark theme colors
+import FlashList from "@shopify/flash-list/dist/FlashList";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+
+const DATA = [
+  { id: "1", title: "Stat 1" },
+  { id: "2", title: "Stat 2" },
+  { id: "3", title: "Stat 3" },
+];
 
 export default function ProfileScreen() {
   return (
@@ -16,42 +25,22 @@ export default function ProfileScreen() {
       title="Profile"
       subtitle="Welcome to the Profile Screen"
       >
-      <ThemedText type="title">Profile</ThemedText>
-      {/* Using TwoColumnLayout with icon theme colors */}
-      <TwoColumnLayout
-        verticalContainerContent={<ThemedText>BOOBA</ThemedText>}
-        horizontalContainerContents={[
-          <ThemedText>ZOOBA</ThemedText>,
-          <ThemedText>GOOBA</ThemedText>,
-        ]}
-        lightColor={Colors.light.icon} // Pass light theme icon color
-        darkColor={Colors.dark.icon} // Pass dark theme icon color
-      />
-      <ThemedText type="subtitle">Stuff</ThemedText>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>Stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>More Stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>Even Stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText> The Stuff Ends</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>Just kidding lol! MORE Stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>And more stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>And even more stuff</ThemedText>
-      </ThemedMainContainer>
-      <ThemedMainContainer variant={"inContainer"}>
-        <ThemedText>And the stuff ends here</ThemedText>
-      </ThemedMainContainer>
+        <FlashList
+                data={DATA}
+                renderItem={({ item }) => (
+                  <Link asChild href="../stats/stats">
+                    <Pressable onPress={() => console.log(item.title)}>
+                      <ThemedMainContainer variant={"default"}>
+                        <ThemedView variant={"inContainer"}>
+                          <ThemedText>{item.title}</ThemedText>
+                        </ThemedView>
+                      </ThemedMainContainer>
+                    </Pressable>
+                  </Link>
+                )}
+                estimatedItemSize={10}
+                ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+              />
     </ParallaxScrollView>
   );
 }
--- constants/icon.tsx
+++ constants/icon.tsx
@@ -1,8 +1,12 @@
-import { Feather, Ionicons } from "@expo/vector-icons";
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
 
 export const icon = {
-  index: (props: any) => <Ionicons name="home" size={24} {...props} />,
-  explore: (props: any) => <Ionicons name="compass" size={24} {...props} />,
-  stats: (props: any) => <Feather name="pie-chart" size={24} {...props} />,
+  index: (props: any) => (
+    <Ionicons name="barbell-outline" size={24} {...props} />
+  ),
+  explore: (props: any) => (
+    <FontAwesome6 name="bowl-food" size={24} {...props} />
+  ),
+  stats: (props: any) => <FontAwesome6 name="spa" size={24} {...props} />,
   profile: (props: any) => <Ionicons name="person" size={24} {...props} />,
 };
