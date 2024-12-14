0ab4420fad68bb5e209551eb01a6abda125d5f4c
AnthonyBeerus <anthony.morima@gmail.com>
2024-12-15 01:51:37 +0200

    style: height change from 200 to 220

--- components/WorkoutOverview.tsx
+++ components/WorkoutOverview.tsx
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     gap: 10,
-    height: 200,
+    height: 220,
   },
   verticalContainer: {
     flex: 1,
