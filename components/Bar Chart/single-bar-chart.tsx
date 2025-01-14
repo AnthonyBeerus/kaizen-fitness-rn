053a1a857a3ba796e02f61d994c06a746788590c
AnthonyBeerus <anthony.morima@gmail.com>
2025-01-14 18:42:10 +0200

    feat: add SingleBarChart component for animated bar chart visualization
    - Introduced the `SingleBarChart` component, which renders an animated bar chart representing data for a single day.
    - Implemented animation using `react-native-reanimated` to smoothly update bar height and opacity based on `day.value`.
    - Included support for theming with `useThemeColor`, allowing dynamic light and dark mode bar colors.
    - Utilized `date-fns` for formatting the day label into a single-letter representation (e.g., "M", "T").
    - Added customizable styles for bar dimensions (`maxHeight`, `width`) and corner radius.
    - Included `ThemedText` for consistent text styling, supporting lowercase day labels in "FiraCode-Regular" font.
    
    This component enhances data visualization capabilities with smooth animations and theme-aware design.

---
+++ components/Bar Chart/single-bar-chart.tsx
@@ -0,0 +1,69 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { format } from "date-fns";
+import React from "react";
+import { Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { ThemedText } from "../ThemedText";
+
+export type Day = {
+  day: Date;
+  value: number; // 0 - 1
+};
+
+type SingleBarChartProps = {
+  maxHeight: number;
+  width: number;
+  day: Day;
+  lightColor?: string;
+    darkColor?: string;
+};
+
+export const SingleBarChart = ({
+  maxHeight,
+  width,
+  day,
+    lightColor,
+    darkColor,
+}: SingleBarChartProps) => {
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      height: withTiming(maxHeight * day.value),
+      opacity: withTiming(day.value),
+    };
+  }, [day.value, maxHeight]);
+
+  const barColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "brandColor"
+  );
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          {
+            width: width,
+            backgroundColor: barColor,
+            borderRadius: 15,
+            borderCurve: "continuous",
+          },
+          rStyle,
+        ]}
+      />
+      <ThemedText
+        style={{
+          width: width,
+          textAlign: "center",
+          fontSize: 12,
+          marginTop: 5,
+          fontFamily: "FiraCode-Regular",
+          textTransform: "lowercase",
+        }}>
+        {format(day.day, "eeeee")}
+      </ThemedText>
+    </View>
+  );
+};
